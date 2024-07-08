e375698f9e545dc65fa4da9d83846c3b9b68906b
Travis Hilterbrand <travis.hilterbrand@zingly.ai>
2024-07-08 16:39:49 -0700

    update to v5

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PokeCard } from "./components/PokeCard";
 import { PokeList } from "./components/PokeList";
 
--- src/hooks/usePokemon.ts
+++ src/hooks/usePokemon.ts
@@ -1,9 +1,10 @@
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 import { Pokemon, getPokemon } from "../api/pokemon";
 
 export const usePokemon = (id: string) => {
-  const { data, error, isLoading } = useQuery<Pokemon>(["pokemon", id], () =>
-    getPokemon(id)
-  );
+  const { data, error, isLoading } = useQuery<Pokemon>({
+    queryKey: ["pokemon", id],
+    queryFn: () => getPokemon(id),
+  });
   return { data, error, isLoading };
 };
